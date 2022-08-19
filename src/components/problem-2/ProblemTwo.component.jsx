3d84f845cf3a07f3b64ab5044dd782ea3d883388
Argel Miralles <argelmiralles25@gmail.com>
2022-08-19 14:50:50 +0800

    edited JS problem section header

--- src/components/problem-2/ProblemTwo.component.jsx
+++ src/components/problem-2/ProblemTwo.component.jsx
@@ -4,16 +4,19 @@ export default function ProblemTwo() {
   return (
     <div className="min-vh-100 d-flex align-items-center justify-content-center flex-column">
       <h2 className="my-3 text-orange display-5">JavaScript Problems</h2>
-      <h3 className="my-3 mb-5">
-        Codepen link:{" "}
-        <a
-          href="https://codepen.io/rgel25/pen/QWmzmdm"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          https://codepen.io/rgel25/pen/QWmzmdm
-        </a>
-      </h3>
+      <div className="col-12 text-wrap text-center p-3">
+        <h2 className="mb-5 fs-5 text-wrap mx-auto">
+          Codepen link:{" "}
+          <a
+            href="https://codepen.io/rgel25/pen/QWmzmdm"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="fs-5 text-wrap text-break"
+          >
+            https://codepen.io/rgel25/pen/QWmzmdm
+          </a>
+        </h2>
+      </div>
       <div className="row w-100">
         <div className="col-12 col-md-6 text-center bg-white">
           <img
